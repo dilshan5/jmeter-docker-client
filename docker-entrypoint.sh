b91f2e796e4836c73a66165a8e6229da4fbdfbdf
Dilshan F <dilshan.fernando@sysco.com>
2020-11-08 11:17:08 +0530

    print JMeter arguments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,12 +10,13 @@ x=$(($freeMem/10*8))
 n=$(($freeMem/10*2))
 export JVM_ARGS="-XX:+AggressiveOpts -Xmn${n}m -Xms${s}m -Xmx${x}m"
 #export JVM_ARGS="-XX:ParallelGCThreads=8 -XX:-UseG1GC -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+UseLargePages -Xmn${n}m -Xms${s}m -Xmx${x}m"
+#export JVM_ARGS="-XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -Xmn${n}m -Xms${s}m -Xmx${x}m"
 
 echo "JAVA JDK Version: ${JAVA_JDK_VERSION}"
 echo "JMeter Version: ${JMETER_VERSION}"
 echo "JMeter Plugins Installed: ${JMETER_PLUGINS}"
-echo "Start Running Jmeter Docker with Time Format : ${TZ} : `date`"
-
+echo "jmeter args=$@"
+echo "------ Start Running Jmeter Docker with Time Format : ${TZ} : `date` -------"
 # Keep entrypoint simple: we must pass the standard JMeter arguments
 jmeter $@
 
